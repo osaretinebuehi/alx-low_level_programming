d33aeb0f5d2049b1894a17bf960fb729e029e3a2
osaretinebuehi2007@gmail.com <osaretinebuehi2007@gmail.com>
2023-03-16 17:16:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  * main - this is the main function
  *
  * Return: always 0
+ */
 
-int main{void}
+int  main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
