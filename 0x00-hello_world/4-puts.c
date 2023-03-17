111bcca00a18487cd0758fd2acf3830a27b5fd4a
osaretinebuehi2007@gmail.com <osaretinebuehi2007@gmail.com>
2023-03-17 09:57:13 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - this is the main function
  *
  * Return: always 0
- */
-
-int main(void) 
+*/
+intmain(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
